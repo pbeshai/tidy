2d5ca0fa2ff88a8a8235e6efe530ef0f4f80e719
Peter Beshai <pbeshai@netflix.com>
2021-02-03 15:34:19 -0800

    refactor: fix types, do not allocate array for n

--- packages/tidy/src/helpers/summation.ts
+++ packages/tidy/src/helpers/summation.ts
@@ -8,8 +8,9 @@ export function fcumsum<T>(
   let sum = new Adder(),
     i = 0;
 
-  return Float64Array.from(items, (value) =>
-    sum.add(+accessor(value, i++, items) || 0)
+  return Float64Array.from(
+    items,
+    (value: T): number => +sum.add(+(accessor(value, i++, items) || 0))
   );
 }
 
@@ -17,10 +18,14 @@ export function mean<T>(
   items: T[],
   accessor: (element: T, i: number, array: Iterable<T>) => any
 ): number | undefined {
-  const n = items.filter((datum, i) => {
-    const value = accessor(datum, i, items);
-    return +value === value;
-  }).length;
+  let n = 0;
+  for (let i = 0; i < items.length; ++i) {
+    const value = accessor(items[i], i, items);
+    // count it if we have a valid number
+    if (+value === value) {
+      n += 1;
+    }
+  }
 
   return n ? fsum(items, accessor) / n : undefined;
 }
