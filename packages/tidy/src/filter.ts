149fc1d18ef897f775b9bdfaa69d67ed2c930c91
Markus J. Ankenbrand <iimog@iimog.org>
2021-05-12 16:49:27 +0200

    feat: allow type guards for filter (#39) (#40)

--- packages/tidy/src/filter.ts
+++ packages/tidy/src/filter.ts
@@ -4,6 +4,12 @@ import { TidyFn } from './types';
  * Filters items
  * @param filterFn Returns true to keep the item, false to filter out
  */
+export function filter<T extends object, O extends T>(
+  filterFn: (item: T, index: number, array: T[]) => item is O
+): TidyFn<T, O>;
+export function filter<T extends object>(
+  filterFn: (item: T, index: number, array: T[]) => boolean
+): TidyFn<T>;
 export function filter<T extends object>(
   filterFn: (item: T, index: number, array: T[]) => boolean
 ): TidyFn<T> {
