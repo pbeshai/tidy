aacb62d5c662bcbd60548282d67f9cb26ff078e1
Peter Beshai <pbeshai@netflix.com>
2022-06-22 09:41:06 -0700

    fix: types of first and last as any

--- packages/tidy/src/summary/first.ts
+++ packages/tidy/src/summary/first.ts
@@ -2,9 +2,8 @@
  * Returns a function that returns the first value for the specified key
  * @param key A string key of the object or an accessor converting the object to a number
  */
-export function first<T extends object>(key: keyof T | ((d: T) => number)) {
-  const keyFn =
-    typeof key === 'function' ? key : (d: T) => (d[key] as unknown) as number;
+export function first<T extends object>(key: keyof T | ((d: T) => any)) {
+  const keyFn = typeof key === 'function' ? key : (d: T) => d[key];
 
   return (items: T[]) => (items.length ? keyFn(items[0]) : undefined);
 }
--- packages/tidy/src/summary/last.ts
+++ packages/tidy/src/summary/last.ts
@@ -2,9 +2,8 @@
  * Returns a function that returns the last value for the specified key
  * @param key A string key of the object or an accessor converting the object to a number
  */
-export function last<T extends object>(key: keyof T | ((d: T) => number)) {
-  const keyFn =
-    typeof key === 'function' ? key : (d: T) => (d[key] as unknown) as number;
+export function last<T extends object>(key: keyof T | ((d: T) => any)) {
+  const keyFn = typeof key === 'function' ? key : (d: T) => d[key];
 
   return (items: T[]) =>
     items.length ? keyFn(items[items.length - 1]) : undefined;
