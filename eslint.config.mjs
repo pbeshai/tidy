7768f1d15483a2d6d10637fe289398ad0582c71c
Peter Beshai <pbeshai@netflix.com>
2026-03-13 15:33:43 -0700

    chore: upgrade eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/', '**/node_modules/', 'website/'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      'prefer-const': 'off',
+      'no-useless-assignment': 'off',
+      'no-empty': 'warn',
+      'no-redeclare': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: false,
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.test.js', '**/*.test.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+      'no-empty': 'off',
+    },
+  }
+);
